8b8a957d5b852010c4f1b431ea7f1665b29d1947
Kapil Rathore <krkapilrathore@gmail.com>
2015-11-03 08:44:00 +0530

    Create ViewController.swift

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Kapil Rathore on 13/08/15.
+//  Copyright (c) 2015 Kapil Rathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        funFactLabel.text = factBook.randomFact()
+        
+        let randomColor = colorWheel.randomColor()
+        funFactButton.tintColor = randomColor
+        view.backgroundColor = randomColor
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFacts() {
+        
+        let randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+
+}
+
